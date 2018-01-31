5e70eb75b8587ba644a017795f033b9a720b79fb
pablodelarosam <pablo.delarosaml@udlap.mx>
2018-01-30 22:23:26 -0600

    front

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,93 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+    isGoing: true,
+    numberOfGuests: 2
+  };
+
+  this.handleInputChange = this.handleInputChange.bind(this);
+}
+
+handleInputChange(event) {
+  const target = event.target;
+  const value = target.type === 'checkbox' ? target.checked : target.value;
+  const name = target.name;
+
+  this.setState({
+    [name]: value
+  });
+}
+
+
+
+  sending = () => {
+
+    console.log("ok")
+}
+
+  submitInfo = () => {
+
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+      <h1> Congreso de Economía, Banca e Inversiones </h1>
+
+      <form>
+   <label>
+    Nombre:
+     <input
+       name="name"
+       type="text"
+       checked={this.state.isGoing}
+       onChange={this.handleInputChange} />
+   </label>
+   <br />
+   <label>
+        Carrera:
+     <input
+       name="career"
+       type="text"
+    
+       onChange={this.handleInputChange} />
+   </label>
+   <br />
+   <label>
+        Semestre:
+     <input
+       name="semester"
+       type="text"
+
+       onChange={this.handleInputChange} />
+   </label>
+   <br />
+   <label>
+        Correo:
+     <input
+       name="email"
+       type="text"
+
+       onChange={this.handleInputChange} />
+   </label>
+   <button> Registrar </button>
+ </form>
+
+ <div className="background">
+
+  </div>
+
       </div>
+
+
     );
   }
 }
