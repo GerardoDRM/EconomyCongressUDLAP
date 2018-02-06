a8df4fecef33ff14eca620127fb2c94bf22f8aa1
Ubuntu <ubuntu@ip-172-31-36-12.us-east-2.compute.internal>
2018-02-06 07:29:54 +0000

    Testing

--- backend/congress/congress/congress.py
+++ backend/congress/congress/congress.py
@@ -8,8 +8,8 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = ''
-app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_USERNAME'] = 'economiaudlap@gmail.com'
+app.config['MAIL_PASSWORD'] = 'NosotrosContraTodos'
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 mail=Mail(app)
 
